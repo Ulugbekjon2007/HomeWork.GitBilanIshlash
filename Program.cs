7fee8c63d41322cef0ec37c4efa1365a0ae7a846
Ulugbekjon2007 <safarovulugbekjon7@gmail.com>
2024-12-05 13:18:05 +0500

    salom

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace GitBilanIshlash
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hammaga salom");
         }
     }
 }
